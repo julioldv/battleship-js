4f017cee9ae53bc143686dbf95cbf5d1efe848ea
Julio Cesar <172700125+julioldv@users.noreply.github.com>
2026-07-27 18:12:25 -0600

    feat: render ship and missed attacks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import "modern-normalize";
 import "./styles.css";
 
+import { createGame } from "./modules/game.js";
 import { renderBoard } from "./modules/dom.js";
+
+const game = createGame();
+game.humanAttack([5, 5]); // Hits the computer ship
+game.humanAttack([0, 0]); // Misses
 
 const humanBoardElement = document.querySelector("#human-board");
 const computerBoardElement = document.querySelector("#computer-board");
 
-renderBoard(humanBoardElement);
-renderBoard(computerBoardElement);
+renderBoard(game.humanPlayer.gameboard, humanBoardElement, true);
+
+renderBoard(game.computerPlayer.gameboard, computerBoardElement, false);
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,12 +1,25 @@
-const renderBoard = function (container) {
+const renderBoard = function (gameboard, container, showShips) {
   container.replaceChildren();
+
   for (let row = 0; row < 10; row++) {
     for (let column = 0; column < 10; column++) {
       const button = document.createElement("button");
+
       button.type = "button";
       button.classList.add("cell");
       button.dataset.row = row;
       button.dataset.column = column;
+
+      const ship = gameboard.getCell([row, column]);
+      const attacked = gameboard.wasAttacked([row, column]);
+
+      if (attacked && ship !== null) {
+        button.classList.add("hit");
+      } else if (attacked) {
+        button.classList.add("miss");
+      } else if (ship !== null && showShips) {
+        button.classList.add("ship");
+      }
 
       container.append(button);
     }
